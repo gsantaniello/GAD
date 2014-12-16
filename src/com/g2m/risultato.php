5b527ae7cf9e87e7e4e98886501d0283c1f69bbb
piegius <Giuseppe@GIUSEPPE-PC>
2014-12-16 10:32:20 +0100

    modifica wrappers, home (get) e risultato.php

--- src/com/g2m/risultato.php
+++ src/com/g2m/risultato.php
@@ -1,21 +1,33 @@
 <html>
 <head>
+<meta charset="utf-8"/>
 <title>Music G2M</title>
+
 </head>
 <?php  
 include("WrapperMusixMatch.php");
 include("WrapperLastFm.php");
-$artist=$_POST['artist'];
-$song=$_POST['song'];
+$artist=$_GET['artist'];
+$song=$_GET['song'];
 
 $wrapperMusixMatch = new WrapperMusixMatch($artist, $song);
-$textSong = $wrapperMusixMatch->scrapingText();
-
-echo "<div id='testo'> <pre>".htmlspecialchars_decode($textSong)."</pre></div>";
-
 $wrapperLastFm = new WrapperLastFm($artist, $song);
-$infoSong = $wrapperLastFm->scrapingInfoSong();
 
-echo $infoSong;
+
+if($artist == null && $song != null){
+	$listSongs = $wrapperLastFm->getListSongs();
+	
+	echo $listSongs;
+}
+
+if($artist != null && $song != null){
+	$textSong = $wrapperMusixMatch->scrapingText();
+	
+	echo "<div id='testo'> <pre>".$textSong."</pre></div>";
+	
+	$infoSong = $wrapperLastFm->getInfoSong();
+	
+	echo $infoSong;
+}
 	
 ?>
